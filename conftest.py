b972c4110cde9a4e2b609e538fbc29edc1060ec5
imperuminate <imperuminate@gmail.com>
2025-03-12 18:28:29 -0600

    headless added

--- conftest.py
+++ conftest.py
@@ -1,14 +1,22 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from pages.customer_login_page import CustomerLoginPage
 from pages.sale_page import SalePage
 
 
 @pytest.fixture
 def driver():
-    chrome_driver = webdriver.Chrome()
-    chrome_driver.maximize_window()
+    # chrome_driver = webdriver.Chrome()
+    # chrome_driver.maximize_window()
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    chrome_driver = webdriver.Chrome(options=options)
     return chrome_driver
+
+
 
 
 @pytest.fixture()
