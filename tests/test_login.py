4f601e12762fef0a6195e30ecc7f847ded8d0e4f
imperuminate <imperuminate@gmail.com>
2025-03-12 18:48:16 -0600

    mark

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,3 +1,7 @@
+import pytest
+
+
+@pytest.mark.smoke
 def test_incorrect_login(customer_login_page):
     customer_login_page.open_page()
     customer_login_page.fill_login_form('notexistingemail@gmail.com', 'totvac@gmail.com')
@@ -6,6 +10,7 @@ def test_incorrect_login(customer_login_page):
     )
 
 
+@pytest.mark.regression
 def test_correct_email_with_incorrect_password(customer_login_page):
     customer_login_page.open_page()
     customer_login_page.fill_login_form('totvac@gmail.com', 'totvac@gmail.com')
--- tests/test_sale_page.py
+++ tests/test_sale_page.py
@@ -1,3 +1,7 @@
+import pytest
+
+
+@pytest.mark.extended
 def test_sale_header(sale_page):
     sale_page.open_page()
     sale_page.check_header('Sale')
